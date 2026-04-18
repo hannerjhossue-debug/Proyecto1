d5f21aed5b31236e5939b61e5bbe92a7cf44445c
hannerjhossue-debug <hannerjhossue@gmail.com>
2026-04-18 14:59:49 -0400

    Update database.js

--- database.js
+++ database.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
-const link = "mongodb+srv://proyectolooksmax_db_user:cnfKWFOZaUskQyUn@cluster0.jin1qv9.mongodb.net/?appName=Cluster0
-‎"; // El que sacamos de Atlas
+
+// Esta es tu línea configurada con tu nueva contraseña y los ajustes de estabilidad
+const link = "mongodb+srv://proyectolooksmax_db_user:32953738@cluster0.jin1qv9.mongodb.net/?retryWrites=true&w=majority";
 
 const conectarDB = async () => {
     try {
         await mongoose.connect(link);
-        console.log('✅ Base de Datos Conectada');
-    } catch (e) { console.log(e); }
+        console.log('✅ [DATABASE] Base de datos Maruchan conectada con éxito');
+    } catch (e) {
+        console.log('❌ [DATABASE] Error de conexión: ' + e);
+    }
 };
+
 module.exports = conectarDB;
